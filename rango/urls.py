c98a4b9ca51ca93b5fb254b462cbf47d04cdf336
kaikanwu <wukaikan@gmail.com>
2018-01-30 21:24:49 +0000

    ch8

--- rango/urls.py
+++ rango/urls.py
@@ -8,6 +8,8 @@ from rango import views
 #         views.show_category, name='show_category'),
 # ]
 
+app_name = 'rango'
+
 urlpatterns = [url(r'^$', views.index, name='index'),
                url(r'^about/$', views.about, name='about'),
                url(r'^add_category/$', views.add_category, name='add_category'),
